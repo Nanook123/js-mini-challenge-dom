aee560f83f4330806f0902adee4c0a98021ef39d
Joshua Allford <joshallfordnanook@gmail.com>
2021-09-03 16:49:19 +0100

    pushing

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 /***** Deliverable 1 *****/
-const header = document.querySelector("#header")
+const header = document.querySelector("h1")
 console.log("Here's your header:", header)
 
 
 /***** Deliverable 2 *****/
 
+document.getElementById("header").style.color = 'red'
+
 
 /***** Deliverable 3 *****/
-// console.log("PLAYERS array looks like this:", PLAYERS)
+//console.log("PLAYERS array looks like this:", PLAYERS)
+
+const playerContainer = document.querySelector('div')
+
+console.log(playerContainer)
+
+
 
 
 /***** Deliverable 4 *****/
